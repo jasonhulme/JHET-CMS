6cc4231fd81b57d16d869c994d363a077079086e
Jason Hulme <jason@bso.net.au>
2011-08-22 18:29:18 +1000

    1

---
+++ plugins/page/page.js
@@ -0,0 +1,35 @@
+
+exports.page  = 
+{
+	exists: function(siteID,pageName)
+	{
+		return true;	
+	},
+	
+	get: function(siteID,pageTitle)
+	{
+		var db = dbconn.connect("cms","page");		
+		db.find({pageTitle:pageTitle.toString(),siteID:siteID.toString()}).toArray(function(err, items)
+		{				
+			if (items==null) 
+			{
+				return null;					
+			}
+			else
+			{
+				return items[0].content;															
+			}
+		});
+	}
+	
+	update: function() 
+	{
+	 	
+	}
+	
+	create: function(siteID,pageTitle,content,subof)
+	{
+			
+	}
+
+};
